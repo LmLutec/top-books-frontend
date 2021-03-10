4a262edbd1edb7552dfd1c725405d3fc163f2fe7
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-03-10 18:04:18 -0500

    installed react-router-dom

---
+++ BookContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+import Book from './book';
+
+class BookContainer extends Component {
+    render(){
+        return(
+            <div>
+                <Book />
+            </div>
+        )
+    }
+}
---
+++ book.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Book extends Component {
+    render(){
+        return(
+            <div>
+                <p>Rate this book</p>
+            </div>
+        )
+    }
+}
+
+export default Book; 
