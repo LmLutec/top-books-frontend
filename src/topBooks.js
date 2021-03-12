31c2bf1c2a95b9dbcf32e47a691bcaf588108d8f
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-03-12 06:35:23 -0500

    set up the displayBooks method. Fixed coding problem by using flat()

--- src/topBooks.js
+++ src/topBooks.js
@@ -5,44 +5,48 @@ import React, { Component } from 'react';
 
 class TopBooks extends Component {
 
-  state = {};
+  state = {
+      books: []
+  };
     
     
 
-    getTopBooks(){
+    componentDidMount(){
         fetch("https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=pccZUGYERtQZfu94bOCqSmWa6RaGL10Q")
             .then(response => {
                 return response.json();
             })
             .then(json => {
-                addBooksToArray(json.results);
+                console.log('data received')
+                this.state.books.push(json.results)
+                this.displayBooks()
+                // this.addBooksToArray(json.results);
             });
     }
 
-    addBooksToArray (arr){
-        arr.map((element) => {
-            books.push(element)
-        }); 
-
+    addBooksToArray = (arr) => {
+        return arr.map((element, id) => <li key={id}>{element.title}</li>); 
+    };
         
-    function displayBooks(){
-        books.forEach((book)=> {
-            console.log(book);
+    displayBooks = () => {
+        console.log(this.state.books.flat())
+        this.state.books.map((book)=> {
+            console.log(book[0].title);
         });
     }
         
-}
+
     render(){
         return(
             <div>
                 <h1>Top Books</h1>
-                {getTopBooks()}
+                
             </div>
         )
     }
-
+};
 
         
-};
+
 
 export default TopBooks;
